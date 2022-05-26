59740ff6632619ef260b364017bdbf6f246ff0cc
Leo Niemi <niemi.leo@gmail.com>
2022-05-26 11:29:16 +0300

    Restructure tests

--- test/example.test.mjs
+++ test/example.test.mjs
@@ -2,18 +2,20 @@ import { expect } from "chai";
 import { Board } from "../src/GameOfLife.mjs";
 
 describe("A board can be created:", () => {
-  it("With an array as a parameter", () => {
-    const array = [
-      `000`,
-      `010`,
-      `000`
-    ]
-    let board = new Board(array);
-    expect(board.toString()).to.equal(
-`000
+  describe("With an array as a parameter", () => {
+    let board;
+    beforeEach(() => {
+      const array = [`000`, `010`, `000`];
+      board = new Board(array);
+    });
+    it("Board.toString() returns a formatted string", () => {
+      expect(board.toString()).to.equal(
+        `000
 010
 000
 `
-    )
+      );
+    });
+    it("Each cell is an integer", () => {});
   });
 });
