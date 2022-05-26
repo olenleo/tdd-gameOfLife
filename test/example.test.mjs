532a5837278abad03e064bfab95fd2d4bb50339f
Leo Niemi <niemi.leo@gmail.com>
2022-05-26 12:17:10 +0300

    Start working on .rle imports

--- test/example.test.mjs
+++ test/example.test.mjs
@@ -2,8 +2,8 @@ import { expect } from "chai";
 import { Board } from "../src/GameOfLife.mjs";
 
 describe("A board can be created:", () => {
+  let board;
   describe("With an array as a parameter", () => {
-    let board;
     beforeEach(() => {
       const array = [`000`, `010`, `000`];
       board = new Board(array);
@@ -16,9 +16,7 @@ describe("A board can be created:", () => {
 `
       );
     });
-    // I misunderstood the mocha syntax here.
-    // Time to fix the test
-    // Even though it should go the other way around usually.
+   
     it("Each cell is an integer", () => {
       for (let i = 0; i < board.getRows(); i++) {
         for (let j = 0; j < board.getCols(); j++) {
@@ -26,5 +24,22 @@ describe("A board can be created:", () => {
         }
       }
     });
+    // Actually we could perform a parsing of the RLE file to an array
+    // And then use the board(array) method.
   });
+  
+  // The parsing of RLE files might be a good idea to implement early.
+  // That way we can get test material without manually writing a lot of arrays.
+  describe("The GameOfLife class can parse RLE files", () => {
+    const blockFile = "./patterns/block.rle";
+    const blockRLE = "#N Block\n#C An extremely common 4-cell still life.\n#C www.conwaylife.com/wiki/index.php?title=Block";
+    const blinkerFile = "./patterns/blinker.rle";
+    const gliderFile = "./patterns/glider.rle";
+
+    it("GameOfLife() can read an RLE file header", () => {
+      board = new Board(blockFile);
+      expect(board.getRLEHeader()).to.equal(blockRLE)
+    })
+  })
+  
 });
