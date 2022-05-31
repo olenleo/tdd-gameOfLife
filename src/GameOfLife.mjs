0e4069b0b35b6e3f917b106f215728a5013c7231
Leo Niemi <niemi.leo@gmail.com>
2022-05-31 18:03:16 +0300

    Add test for reading ./result.rle

--- src/GameOfLife.mjs
+++ src/GameOfLife.mjs
@@ -1,5 +1,320 @@
 import fs from "fs";
 
+export class GameOfLife {
+  #tickLimit;
+  #tick;
+  #file;
+  #board;
+  #fileData;
+  #RLEHeader = "";
+  #RLEarray = [];
+  #RLEpatternAsString;
+  #fullRLEHeader;
+  #patternWidth;
+  #patternHeight;
+  #finalState = [];
+  #reps = 1;
+  #endline = false;
+
+  constructor(file, tickLimit) {
+    console.log(file)
+    try {
+      fs.readFileSync(file, "utf8");
+    } catch (e) {
+      throw new Error("Error: File does not exist");
+    }
+    this.#file = file;
+    this.#tickLimit = tickLimit;
+    this.#tick = 0;
+    this.readRLE();
+    
+    this.#board = new Board(this.#RLEarray);
+  }
+
+  play() {
+    while (this.#tick < this.#tickLimit) {
+      this.#board.tick();
+      this.#tick++;
+    }
+    this.#finalState = this.#board;
+    this.writeResultFile()
+    return this.#board.toString();
+  }
+  // Edit this to make sure the 10x10 pattern is present in dimensions.
+  writeResultFile() {
+    const path = "./result.rle";
+    this.#RLEpatternAsString = this.arrayToString();
+    const xRegex = /x\s=\s\d+/;
+    const yRegex = /y\s=\s\d+/;
+    let y = this.#fullRLEHeader.match(yRegex)[0].match(/\d/);
+    let x = this.#fullRLEHeader.match(xRegex)[0].match(/\d/);
+    this.#fullRLEHeader = this.#fullRLEHeader.replace(y, 10)
+    this.#fullRLEHeader = this.#fullRLEHeader.replace(x, 10)
+    let header = this.#fullRLEHeader +  this.parseArrayToRLE();
+    console.log('Header', header)
+    try {fs.writeFileSync(path, header)} catch (e) {console.log(e)}
+    
+  }
+
+  arrayToString() {
+    let s = ""
+    for (let row = 0; row < 10; row++) {
+      for (let col = 0; col < 10; col++) {
+        s += this.#board.getStateOfCell(row,col)
+      }
+    }
+    
+    return s
+    }
+
+  getFinalState() {
+    return this.#finalState.toString();
+  }
+  getRLEarray() {
+    return this.#RLEarray;
+  }
+  getRLEHeader() {
+    return this.#RLEHeader;
+  }
+  getRLEpatternAsString() {
+    return this.#RLEpatternAsString;
+  }
+  getX() {
+    return this.#patternWidth;
+  }
+  getY() {
+    return this.#patternHeight;
+  }
+
+  getBoard() {
+    return this.#board;
+  }
+
+  readRLE() {
+    try {
+      const data = fs.readFileSync(this.#file, "utf8");
+      this.#fileData = data;
+      console.log('ReadRLE method\nfile:', this.#file, "\n", data)
+      this.parseRLE();
+    } catch (err) {
+      console.error(err);
+    }
+  }
+  
+  parseRLE() {
+    const headerRegex = /#.+[$|\n]/g;
+    const xRegex = /x\s=\s\d+/;
+    const yRegex = /y\s=\s\d+/;
+    const patternRegex = /rule\s=\s.*\n([^\r\n]+)/;
+    const ruleRegex = /rule\s=\s.+[\n|$]/
+    const arr = this.#fileData.match(headerRegex);
+    this.#patternHeight = parseInt(
+      this.#fileData.match(yRegex)[0].match(/\d/));
+    this.#patternWidth = parseInt(
+      this.#fileData.match(xRegex)[0].match(/\d/));
+    this.#RLEpatternAsString = this.#fileData.match(patternRegex)[1];
+    this.#RLEarray = this.parseRLEtoArray();
+    for (let i in arr) {
+      this.#RLEHeader += arr[i];
+    }
+    this.#fullRLEHeader = this.#RLEHeader + this.#fileData.match(xRegex)[0] + " " + this.#fileData.match(yRegex)[0] + " "
+    + this.#fileData.match(ruleRegex);
+    console.log('The full RLE header: ' , this.#fullRLEHeader)
+  }
+
+
+  parseArrayToRLE() {
+    const s = this.#RLEpatternAsString;
+    console.log('Parse array to rle recieves: ', s)
+    let rle = "";
+    
+    let curr = s[0];
+    let next = s[1];
+    
+    for (let i = 1; i < s.length; i++) {
+      if (i % 10 === 0 && i !== 0) {
+        this.#endline = true;
+        rle += this.handleInsert(curr, next,  this.#reps, i);
+        this.#reps = 1;
+      }
+      if (next != curr) {
+        rle += this.handleInsert(curr, next,  this.#reps, i);
+        this.#reps = 1;
+        
+      } else {
+        if (!this.#endline) {
+          this.#reps++;
+        } else {
+          this.#endline = false;
+        }
+    }
+      curr = s[i];
+      next = s[i+1];
+      
+    }
+    rle += this.handleInsert(curr, next,  this.#reps, s.length);
+    return rle + "!\n"
+  }
+
+  handleInsert(curr, next, reps, i) {
+    let val = "";
+    let ret = ""
+    function handleChar(c) {    
+      if (parseInt(c) === 0) { return "b"; } else {return "o"; }
+    }
+    let char = handleChar(curr);
+    let nextChar = handleChar(next)
+    if (reps > 1) {
+      val += reps;
+    }
+    if (reps > 10) { // I placed a ceiling on the 'reps'. Hope it works out.
+      val = 10;  
+    }
+    if (i % 10 === 0 && i !== 0) {
+      if (i == 100) {
+      return val + char;
+      } else {
+        return val + char + "$"
+      }
+    } 
+    ret = char;
+    if (val === 0) {
+      return char;
+    }
+    return val + ret
+    
+  }
+  
+  parseRLEtoArray() {
+    const string = this.#RLEpatternAsString;
+    let rows = string.split("$");
+    let toReturn = [];
+    for (let r = 0; r < rows.length; r++) {
+      toReturn[r] = new Array(this.#patternWidth).fill(0);
+    }
+
+    let repetitions = 0;
+    for (let r = 0; r < rows.length; r++) {
+      let row = [];
+      for (let char = 0; char < this.#patternWidth; char++) {
+        let current = rows[r][char];
+        if (!isNaN(parseInt(current))) {
+          repetitions = parseInt(current);
+        }
+        if (current === "o" || current === "b") {
+          if (repetitions === 0) {
+            row.push(current);
+          } else {
+            for (let i = 0; i < repetitions; i++) {
+              row.push(current);
+            }
+            repetitions = 0;
+          }
+        }
+      }
+      toReturn[r] = this.formatRow(row);
+    }
+    return toReturn;
+  }
+
+  formatRow(row) {
+    for (let i = 0; i < row.length; i++) {
+      if (row[i] === "b") {
+        row[i] = 0;
+      } else {
+        row[i] = 1;
+      }
+    }
+    return row;
+  }
+}
+
+export class Board {
+  #rows;
+  #cols;
+  #board;
+  #file;
+
+  constructor(file) {
+    this.#file = file;
+    if (file instanceof Array) {
+      this.#rows = 10;
+      this.#cols = 10;
+      const patternHeight = file.length;
+      const patternWidth = file[0].length;
+      this.#board = new Array(this.#rows);
+      this.initializeEmptyBoard(this.#board);
+      for (let i = 0; i < patternHeight; i++) {
+        for (let j = 0; j < patternWidth; j++) {
+
+          this.#board[5 - Math.floor(patternWidth / 2) + i][5 - Math.floor(patternHeight / 2) + j] = parseInt(this.#file[i][j]);
+        }
+      }
+    }
+  }
+  initializeEmptyBoard(board) {
+    for (let row = 0; row < this.#rows; row++) {
+      board[row] = new Array(this.#cols).fill(0);
+    }
+    return board;
+  }
+
+  tick() {
+    let next = [];
+    next = this.initializeEmptyBoard(next); // Make changes on empty board
+    for (let row = 0; row < this.#rows; row++) {
+      for (let col = 0; col < this.#cols; col++) {
+        const neighbours = this.countNeigbours(row, col);
+        const cellIsAlive = this.#board[row][col] === 1;
+        if (neighbours < 2 || neighbours > 3) {
+          next[row][col] = 0;
+        } else if (!cellIsAlive && neighbours === 3) {
+          next[row][col] = 1;
+        } else {
+          next[row][col] = this.#board[row][col];
+        }
+      }
+    }
+    this.#board = next; // save changes onto #board.
+  }
+  getStateOfCell(row, col) {
+    return this.#board[row][col]
+  }
+
+  countNeigbours(row, col) {
+    let sum = 0;
+    for (let i = -1; i < 2; i++) {
+      for (let j = -1; j < 2; j++) {
+        let r = (row + i + this.#rows) % this.#rows;
+        let c = (col + j + this.#cols) % this.#cols;
+        sum += parseInt(this.#board[r][c]);
+      }
+    }
+    return sum - parseInt(this.#board[row][col]);
+  }
+
+  toString() {
+    let s = "";
+    for (let i = 0; i < this.#rows; i++) {
+      for (let j = 0; j < this.#cols; j++) {
+        s += this.#board[i][j];
+      }
+      s += "\n";
+    }
+    return s;
+  }
+
+  getRows() {
+    return this.#rows;
+  }
+  getCols() {
+    return this.#cols;
+  }
+  getBoard() {
+    return this.#board;
+  }
+}
+
 // I'm not sure if this is 100% reliable.
 // But since I'm only testing the internal functionality of this function I should be able to copy it into the main program later.
 // Of course I will need to test it in the GameOfLife class context.
@@ -87,259 +402,5 @@ export class TestArrayToRLE {
 
   getStateOfCell(board, row, col) {
     return board[row][col]
-  }
-}
-export class GameOfLife {
-  #tickLimit;
-  #tick;
-  #file;
-  #board;
-  #fileData;
-  #RLEHeader = "";
-  #RLEarray = [];
-  #RLEpatternAsString;
-  #fullRLEHeader;
-  #patternWidth;
-  #patternHeight;
-  #finalState = [];
-
-  constructor(file, tickLimit) {
-    try {
-      fs.readFileSync(file, "utf8");
-    } catch (e) {
-      throw new Error("Error: File does not exist");
-    }
-    this.#file = file;
-    this.#tickLimit = tickLimit;
-    this.#tick = 0;
-    this.readRLE();
-    this.#board = new Board(this.#RLEarray);
-  }
-
-  play() {
-    while (this.#tick < this.#tickLimit) {
-      this.#board.tick();
-      this.#tick++;
-    }
-    this.#finalState = this.#board;
-    this.writeResultFile()
-    return this.#board.toString();
-  }
-
-  writeResultFile() {
-    const path = "./result.rle";
-    const header = this.#fullRLEHeader +  this.parseArrayToRLE();
-    try {fs.writeFileSync(path, header)} catch (e) {console.log(e)}
-    
-  }
-
-  getFinalState() {
-    return this.#finalState.toString();
-  }
-  getRLEarray() {
-    return this.#RLEarray;
-  }
-  getRLEHeader() {
-    return this.#RLEHeader;
-  }
-  getRLEpatternAsString() {
-    return this.#RLEpatternAsString;
-  }
-  getX() {
-    return this.#patternWidth;
-  }
-  getY() {
-    return this.#patternHeight;
-  }
-
-  getBoard() {
-    return this.#board;
-  }
-
-  readRLE() {
-    try {
-      const data = fs.readFileSync(this.#file, "utf8");
-      this.#fileData = data;
-      this.parseRLE();
-    } catch (err) {
-      console.error(err);
-    }
-  }
-
-  parseRLE() {
-    const headerRegex = /#.+[$|\n]/g;
-    const xRegex = /x\s=\s\d+/;
-    const yRegex = /y\s=\s\d+/;
-    const patternRegex = /rule\s=\s.*\n([^\r\n]+)/;
-    const ruleRegex = /rule\s=\s.+[\n|$]/
-    const arr = this.#fileData.match(headerRegex);
-    this.#patternHeight = parseInt(
-      this.#fileData.match(yRegex)[0].match(/\d/)[0]);
-    this.#patternWidth = parseInt(
-      this.#fileData.match(xRegex)[0].match(/\d/)[0]);
-    this.#RLEpatternAsString = this.#fileData.match(patternRegex)[1];
-    this.#RLEarray = this.parseRLEtoArray();
-    for (let i in arr) {
-      this.#RLEHeader += arr[i];
-    }
-    this.#fullRLEHeader = this.#RLEHeader + this.#fileData.match(xRegex)[0] + " " + this.#fileData.match(yRegex)[0] + " "
-    + this.#fileData.match(ruleRegex);
-  }
-
-  reverseString(s) {
-    return s.split("").reverse().join("")
-  }
-  arrayToString() {
-    let s = ""
-    for (let row = 9; row >= 0; row--) {
-      for (let col = 9; col >= 0; col--) {
-        s += this.#board.getStateOfCell(row,col)
-      }
-    }
-    return this.reverseString(s);
-  }
-  
-  parseArrayToRLE( string ) {
-    let s;
-    if (!string) {
-    s = this.arrayToString()
-    } else {
-      s = string;
-    }
-    let rle = "";
-    for (let i = 0; i < s.length; i++) {
-
-    }
-    return rle + "!\n"
-  }
-  
-  parseRLEtoArray() {
-    const string = this.#RLEpatternAsString;
-    let rows = string.split("$");
-    let toReturn = [];
-    for (let r = 0; r < rows.length; r++) {
-      toReturn[r] = new Array(this.#patternWidth).fill(0);
-    }
-
-    let repetitions = 0;
-    for (let r = 0; r < rows.length; r++) {
-      let row = [];
-      for (let char = 0; char < this.#patternWidth; char++) {
-        let current = rows[r][char];
-        if (!isNaN(parseInt(current))) {
-          repetitions = parseInt(current);
-        }
-        if (current === "o" || current === "b") {
-          if (repetitions === 0) {
-            row.push(current);
-          } else {
-            for (let i = 0; i < repetitions; i++) {
-              row.push(current);
-            }
-            repetitions = 0;
-          }
-        }
-      }
-      toReturn[r] = this.formatRow(row);
-    }
-    return toReturn;
-  }
-
-  formatRow(row) {
-    for (let i = 0; i < row.length; i++) {
-      if (row[i] === "b") {
-        row[i] = 0;
-      } else {
-        row[i] = 1;
-      }
-    }
-    return row;
-  }
-}
-
-export class Board {
-  #rows;
-  #cols;
-  #board;
-  #file;
-
-  constructor(file) {
-    this.#file = file;
-    if (file instanceof Array) {
-      this.#rows = 10;
-      this.#cols = 10;
-      const patternHeight = file.length;
-      const patternWidth = file[0].length;
-      this.#board = new Array(this.#rows);
-      this.initializeEmptyBoard(this.#board);
-      for (let i = 0; i < patternHeight; i++) {
-        for (let j = 0; j < patternWidth; j++) {
-          this.#board[5 - Math.floor(patternWidth / 2) + i][
-            5 - Math.floor(patternHeight / 2) + j
-          ] = parseInt(this.#file[i][j]);
-        }
-      }
-    }
-  }
-  initializeEmptyBoard(board) {
-    for (let row = 0; row < this.#rows; row++) {
-      board[row] = new Array(this.#cols).fill(0);
-    }
-    return board;
-  }
-
-  tick() {
-    let next = [];
-    next = this.initializeEmptyBoard(next); // Make changes on empty board
-    for (let row = 0; row < this.#rows; row++) {
-      for (let col = 0; col < this.#cols; col++) {
-        const neighbours = this.countNeigbours(row, col);
-        const cellIsAlive = this.#board[row][col] === 1;
-        if (neighbours < 2 || neighbours > 3) {
-          next[row][col] = 0;
-        } else if (!cellIsAlive && neighbours === 3) {
-          next[row][col] = 1;
-        } else {
-          next[row][col] = this.#board[row][col];
-        }
-      }
-    }
-    this.#board = next; // save changes onto #board.
-  }
-  getStateOfCell(row, col) {
-    return this.#board[row][col]
-  }
-
-  countNeigbours(row, col) {
-    let sum = 0;
-    for (let i = -1; i < 2; i++) {
-      for (let j = -1; j < 2; j++) {
-        let r = (row + i + this.#rows) % this.#rows;
-        let c = (col + j + this.#cols) % this.#cols;
-        sum += parseInt(this.#board[r][c]);
-      }
-    }
-    return sum - parseInt(this.#board[row][col]);
-  }
-
-  toString() {
-    let s = "";
-    for (let i = 0; i < this.#rows; i++) {
-      for (let j = 0; j < this.#cols; j++) {
-        s += this.#board[i][j];
-      }
-      s += "\n";
-    }
-    return s;
-  }
-
-  getRows() {
-    return this.#rows;
-  }
-  getCols() {
-    return this.#cols;
-  }
-  getBoard() {
-    return this.#board;
   }
 }
--- test/example.test.mjs
+++ test/example.test.mjs
@@ -1,18 +1,20 @@
 import { expect } from "chai";
 import { Board, GameOfLife, TestArrayToRLE } from "../src/GameOfLife.mjs";
 import fs from 'fs'
+
+const read = () => {
+  try { 
+    return fs.readFileSync("./result.rle", "utf-8");
+  } catch (e) { throw new Error("Result.rle not created")}
+}
 describe("A board can be created:", () => {
   let board;
-  // I'm thinking the array top left square should be
-  // [rows/2-patternWidth / 2][cols/2 - patternWidth/2]
-  // Apparently I counted wrong again.
-
   describe("With an array as a parameter", () => {
     beforeEach(() => {
       const array = [`111`, `111`, `111`];
       board = new Board(array);
     });
-    it("Board.toString() returns a formatted string", () => {
+   it("Board.toString() returns a formatted string", () => {
       expect(board.toString()).to.equal(
         `0000000000
 0000000000
@@ -28,7 +30,7 @@ describe("A board can be created:", () => {
       );
     });
 
-    it("Each cell is an integer", () => {
+   it("Each cell is an integer", () => {
       for (let i = 0; i < board.getRows(); i++) {
         for (let j = 0; j < board.getCols(); j++) {
           expect(typeof board.getBoard()[i][j]).to.equal(typeof 0);
@@ -43,51 +45,35 @@ describe("A board can be created:", () => {
       "#N Block\n#C An extremely common 4-cell still life.\n#C www.conwaylife.com/wiki/index.php?title=Block\n";
     const blinkerFile = "./patterns/blinker.rle";
     const gliderFile = "./patterns/glider.rle";
-
-    // I'll pause here and read up a bit.
-    // Getting proper files and sanitizing inputs feels very important
-    // On the other hand, I could perhaps trust the user a bit in this case.
-    // I'll make some tests but not too many so I won't drag this out.
-
-    // The mocha documentation is not that easy to navigate.
-    // I found this in the tetris tests though:
-    // expect(() => board.drop(new Block("Y"))).to.throw("already falling");
-    // I think I need to make a separate method for reading the file.
-
-    // I did this backwards. But I'll leave it in anyhow.
-    // It's hard to remember the proper order of operations when a surprising bug / issue comes up.
-
-    // I think my expect() here was a workaround.
-    // Maybe I can test the constructor somehow?
-    // Constructors are functions. I had the wrong syntax here.
-    it("but it throws an error if the file does not exist", () => {
+    
+   it("but it throws an error if the file does not exist", () => {
       const gameOfLife = () => {
         new GameOfLife("Non-existent", 0);
       };
       expect(gameOfLife).to.throw("Error: File does not exist");
     });
-    it("and read an RLE file header", () => {
+   it("and read an RLE file header", () => {
       const gameOfLife = new GameOfLife(blockFile, 0);
       expect(gameOfLife.getRLEHeader()).to.equal(blockRLE);
     });
-    it("and read RLE pattern dimensions", () => {
+   it("and read RLE pattern dimensions", () => {
       const gameOfLife = new GameOfLife(blinkerFile, 0);
       expect(gameOfLife.getX()).to.equal(3);
       expect(gameOfLife.getY()).to.equal(1);
     });
 
-    it("and extract the RLE pattern", () => {
+   it("and extract the RLE pattern", () => {
       const gameOfLife = new GameOfLife(gliderFile, 0);
       expect(gameOfLife.getRLEpatternAsString()).to.equal("bob$2bo$3o!");
     });
 
-    it("and handle parsing the rle syntax", () => {
+   it("and handle parsing the rle syntax", () => {
       const gameOfLife = new GameOfLife(blinkerFile, 0);
       expect(arrayEquals(gameOfLife.parseRLEtoArray(), [[1, 1, 1]])).to.equal(
         true
       );
     });
-    it("and parse the RLE pattern into an array", () => {
+   it("and parse the RLE pattern into an array", () => {
       const gameOfLife = new GameOfLife(gliderFile, 0);
       const gliderArr = [
         [0, 1, 0],
@@ -96,7 +82,7 @@ describe("A board can be created:", () => {
       ];
       expect(arrayEquals(gameOfLife.getRLEarray(), gliderArr)).to.equal(true);
     });
-    it("and import the .rle pattern onto the board class", () => {
+   it("and import the .rle pattern onto the board class", () => {
       const gameOfLife = new GameOfLife(gliderFile, 0);
       const gliderString = `0000000000
 0000000000
@@ -132,24 +118,24 @@ describe("The Game Of Life rules:", () => {
       board = new Board(array);
     });
 
-    it("but a living cell(x,y) is not included in the sum of neighbours", () => {
+   it("but a living cell(x,y) is not included in the sum of neighbours", () => {
       expect(board.countNeigbours(1, 2)).to.equal(3);
       expect(board.countNeigbours(1, 1)).to.equal(5);
       expect(board.countNeigbours(1, 2)).to.equal(3);
     });
-    it("wrapping around over the borders", () => {
+   it("wrapping around over the borders", () => {
       expect(board.countNeigbours(0, 3)).to.equal(2);
       expect(board.countNeigbours(6, 0)).to.equal(6);
     });
   });
   // I think I will simply write new arrays for each test.
   // This takes some more time & adds lines of code
-  // But it will save up some stream recording time as I'm bound to mess up some array if I get too tricky.
+  // Butxit will save up some stream recording time as I'm bound to mess up some array if I get too tricky.
 
   // Due to the update algorithm this does not actually test anything yet.
   // I probably should plan out a good order of tests
   // Or just do several ones that support each other.
-  it("A cell with 0 neighbours dies after a tick()", () => {
+ it("A cell with 0 neighbours dies after a tick()", () => {
     const array = [`000`, `010`, `000`];
     let board = new Board(array);
     board.tick();
@@ -167,7 +153,7 @@ describe("The Game Of Life rules:", () => {
 `
     );
   });
-  it("A cell with 1 neighbour dies after a tick()", () => {
+ it("A cell with 1 neighbour dies after a tick()", () => {
     const array = [`000`, `011`, `000`];
     let board = new Board(array);
     board.tick();
@@ -186,7 +172,7 @@ describe("The Game Of Life rules:", () => {
     );
   });
 
-  it("A cell with more than 3 alive neighbours dies after a tick", () => {
+ it("A cell with more than 3 alive neighbours dies after a tick", () => {
     const array = [
       `0000000000`,
       `0010000000`,
@@ -215,9 +201,7 @@ describe("The Game Of Life rules:", () => {
 `
     );
   });
-  // Well, all I can say is that tests are useful.
-  // Forgot to make sure cells remain alive when conditions are met.
-  it("A dead cell with 3 alive neighbours comes alive after a tick", () => {
+ it("A dead cell with 3 alive neighbours comes alive after a tick", () => {
     const array = [`00000`, `00100`, `00010`, `00100`, `00000`];
     let board = new Board(array);
     board.tick();
@@ -237,11 +221,8 @@ describe("The Game Of Life rules:", () => {
   });
 });
 
-// Time to implement the tick functionality.
-// The game of life is played for n ticks, then create ./result.rle.
-// I will make the tests & functionality for the ticks first.
 describe("The board state updates according to the game of life rules after ticks", () => {
-  it("A block remains unchanged", () => {
+ it("A block remains unchanged", () => {
     let game = new GameOfLife("./patterns/block.rle", 1);
     expect(game.getBoard().toString()).to.equal(
       `0000000000
@@ -257,11 +238,8 @@ describe("The board state updates according to the game of life rules after tick
 `
     );
   });
-  // I'm rushing through this to meet all my deadlines.
-  // This means that I should spend a lot more time making useful, valid tests!
-  // I'll leave a comment in the 'palautussovellus' regarding this.
 
-  it("A blinker changes state", () => {
+ it("A blinker changes state", () => {
     let game = new GameOfLife("./patterns/blinker.rle", 1);
 
     expect(game.play()).to.equal(
@@ -283,46 +261,50 @@ describe("The board state updates according to the game of life rules after tick
 describe("When the tick limit has been reached:", () => {
   let pattern = "./patterns/blinker.rle"
   let game;
-  const read = () => {
-    try { 
-      let data = fs.readFileSync("./result.rle", "utf-8")
-      return data;
-    } catch (e) { throw new Error("Result.rle not created")}
-  }
+  /**
+   * Method returns the contents of './result.rle'
+   * @returns String: result.rle contents
+   */
+
   beforeEach(() => {
-    game = new GameOfLife(pattern,3)
+    game = new GameOfLife(pattern,2)
     try {fs.unlink("./result.rle")} catch(e) {console.log('Before: File already deleted.')}
   });
   afterEach(() => {
     try {fs.unlink("./result.rle")} catch(e) {console.log('After: File already deleted.')}
   });
 
-  it("the file './result.rle' is created", () => {
+ it("the file './result.rle' is created", () => {
     game.play();
     expect(read).to.not.throw()
   });
 
-  it("the file './result.rle' contains a header", () => {
+ it("the file './result.rle' contains a header", () => {
     const header = game.getRLEHeader();
     game.play();
     const resultData = read();
     expect(resultData).to.contain(header);
   })
-  // Without the dimensions and string the game could not be played.
-  // The .rle strings should be identical when the functionality is implemented
-  it("the file './result.rle' can be imported into the program", () => {
-    const modifiedBlinkerRLE = "10b$10b$10b$5b3o2b$10b$10b$10b$10b$10b$10b!\n"
-    game.play();
-    const data1 = read();
-    try {fs.unlink("./result.rle")} catch(e) {console.log('After: File already deleted.')}
-    const newGame = new GameOfLife('./result.rle', 0);
-    newGame.play()
-    const data2 = read();
-    expect(data1).to.equal(data2) // This does not check the content!
-    expect(data1).to.contain(modifiedBlinkerRLE); // If the encoded rle pattern exists, import should work as intended.
-    
 
+  
+  it("the file './result.rle' contains properly formatted dimensions", () => {
+    
+    game.play();
+    const resultData = read();
+    expect(resultData).to.contain("x = 10 y = 10")
   })
+})
+
+
+it("the file './result.rle' can be imported into the program", () => {
+  const game = new GameOfLife("./patterns/blinker.rle",2)
+  const modifiedBlinkerRLE = "10b$10b$10b$10b$5b3o2b$10b$10b$10b$10b$10b!\n"
+  game.play()
+  const data = read();
+  const game2 = new GameOfLife("./result.rle", 2);
+  expect(read()).to.contain(modifiedBlinkerRLE);
+  expect(read()).to.equal(data);
+  try {fs.unlink("./result.rle")} catch (e) {console.log(e)}
 
 })
 
@@ -380,20 +362,20 @@ describe("When writing .rle files", () => {
     [1,0,1,0,1,0,1,0,1,0],
     [0,1,0,1,0,1,0,1,0,1]
   ]  
-  it("An empty board returns the correct string", () => {
+ it("An empty board returns the correct string", () => {
     const test = new TestArrayToRLE(emptyArr)
     expect(test.parseArrayToRLE()).to.equal("10b$10b$10b$10b$10b$10b$10b$10b$10b$10b!\n")
   })
-  it("A full board returns the correct string", () => {
+ it("A full board returns the correct string", () => {
     const test = new TestArrayToRLE(fullArr)
     expect(test.parseArrayToRLE()).to.equal("10o$10o$10o$10o$10o$10o$10o$10o$10o$10o!\n")
   });
   
-  it("A board with no repeats returns a correct string", () => {
+ it("A board with no repeats returns a correct string", () => {
     const test = new TestArrayToRLE(checkers)
     expect(test.parseArrayToRLE()).to.equal("obobobobob$bobobobobo$obobobobob$bobobobobo$obobobobob$bobobobobo$obobobobob$bobobobobo$obobobobob$bobobobobo!\n")
   })
-  it("A rle string is properly parsed", () => {
+ it("A rle string is properly parsed", () => {
     const test = new TestArrayToRLE(blinkerArr)
     expect(test.parseArrayToRLE()).to.equal(blinkerArrToRLE)
   })
